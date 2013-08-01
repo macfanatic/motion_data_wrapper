b96d68f7dddc6e1c47e6ca2e6dcbb9cea43d7fc6
Katsuyoshi Ito <kito@itosoft.com>
2013-08-01 09:22:46 +0900

    add callbacks and fix new_recode? still true after saved.
    I add callbacks before/after_create/save/update/destroy if it exists an instance method.
    Not implemented specify by a class method.
    
    I found a bug, new_recode? still true after saved.

--- lib/motion_data_wrapper/model/persistence.rb
+++ lib/motion_data_wrapper/model/persistence.rb
@@ -32,13 +32,23 @@ module MotionDataWrapper
         end
       
       end
-    
+  
+      def awakeFromFetch
+        after_fetch if respondsToSelector "after_fetch"
+      end
+
+      def awakeFromInsert
+        after_fetch if respondsToSelector "after_fetch"
+      end
+  
       def destroy
       
         if context = managedObjectContext
+          before_destroy_callback
           context.deleteObject(self)
           error = Pointer.new(:object)
           context.save(error)
+          after_destroy_callback
         end
       
         @destroyed = true
@@ -72,14 +82,47 @@ module MotionDataWrapper
           context.insertObject(self)
         end
 
+        before_save_callback
         error = Pointer.new(:object)
         unless context.save(error)
           managedObjectContext.deleteObject(self)
           raise MotionDataWrapper::RecordNotSaved, self and return false
         end
+        instance_variable_set('@new_record', false)
+        after_save_callback
+
         true
       end
-    
+  
+      private
+  
+      def before_save_callback
+        before_save if respondsToSelector "before_save"
+        @is_new_record = new_record?
+        if @is_new_record
+          before_create if respondsToSelector "before_create"
+        else
+          before_update if respondsToSelector "before_update"
+        end
+      end
+  
+      def after_save_callback
+        if @is_new_record
+          after_create if respondsToSelector "after_create"
+        else
+          after_update if respondsToSelector "after_update"
+        end
+        after_save if respondsToSelector "after_save"
+      end
+
+      def before_destroy_callback
+        before_destroy if respondsToSelector "before_destroy"
+      end
+  
+      def after_destroy_callback
+        after_destroy if respondsToSelector "after_destroy"
+      end
+  
     end
   end
 end
